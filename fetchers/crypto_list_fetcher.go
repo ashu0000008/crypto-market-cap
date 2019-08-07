462b0a9ce80cc74fa20b233c275d1eb8d35a17ee
zhouyang <ashu0000008@163.com>
2019-08-07 14:51:10 +0800

    test

---
+++ fetchers/crypto_list_fetcher.go
@@ -0,0 +1,38 @@
+package fetchers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func Fetch() {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	q := url.Values{}
+	q.Add("start", "1")
+	q.Add("limit", "5000")
+	q.Add("convert", "USD")
+
+	req.Header.Set("Accepts", "application/json")
+	req.Header.Add("X-CMC_PRO_API_KEY", "431f8c85-957c-4a08-9f0a-190a58a6a236")
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request to server")
+		os.Exit(1)
+	}
+	fmt.Println(resp.Status)
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(respBody))
+
+}
---
+++ main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ashu0000008/crypto-market-cap/fetchers"
+)
+
+func main() {
+	fetchers.Fetch()
+}
