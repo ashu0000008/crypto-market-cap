bff93a9514ffa61a5f98f565064a93aaa15bbb3c
zhouyang <ashu0000008@163.com>
2019-08-14 16:27:24 +0800

    获取货币列表接口添加

--- api/api.go
+++ api/api.go
@@ -3,13 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/ashu0000008/crypto-market-cap/api/impl"
 )
 
 func main() {
 	// 初始化引擎
 	engine := gin.Default()
 	// 注册一个路由和处理函数
-	engine.Any("/", WebRoot)
+	engine.Any("/", webRoot)
+	engine.GET("/info/list", getCryptoList)
 	// 绑定端口，然后启动应用
 	engine.Run(":80")
 }
@@ -19,6 +22,10 @@ func main() {
 * 所有本次请求相关的方法都在 context 中，完美
 * 输出响应 hello, world
  */
-func WebRoot(context *gin.Context) {
+func webRoot(context *gin.Context) {
 	context.String(http.StatusOK, "hello, world")
+}
+
+func getCryptoList(context *gin.Context) {
+	context.String(http.StatusOK, impl.GetCryptoListImpl())
 }
---
+++ api/impl/get_crypto_list.go
@@ -0,0 +1,51 @@
+package impl
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ashu0000008/crypto-market-cap/db"
+)
+
+func GetCryptoListImpl() string {
+	db, err := db.Setup()
+	if err != nil {
+		print(err)
+		return "db setup error"
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM coininfo")
+	if err != nil {
+		return "db query error:" + err.Error()
+	}
+
+	var coinList []CoinInfo
+	for rows.Next() {
+		var info CoinInfo
+		err = rows.Scan(&info.Cid, &info.Cname, &info.Symbol, &info.Slug, &info.MaxSupply, &info.CirculatingSupply, &info.Platform, &info.Cmcrank)
+		if err != nil {
+			fmt.Print(err.Error())
+			continue
+		}
+		coinList = append(coinList, info)
+	}
+
+	data, err := json.Marshal(coinList)
+	if err != nil {
+		fmt.Println("json marshal failed,err:", err)
+		return "marshal error"
+	}
+
+	return string(data)
+}
+
+type CoinInfo struct {
+	Cid               int         `json:"cid"`
+	Cname             string      `json:"cname"`
+	Symbol            string      `json:"symbol"`
+	Slug              string      `json:"slug"`
+	MaxSupply         interface{} `json:"maxsupply"`
+	CirculatingSupply float64     `json:"circulatingsupply"`
+	Platform          string      `json:"platform"`
+	Cmcrank           int         `json:"cmcrank"`
+}
